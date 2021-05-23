c061f0a84b12cda0dc700539a3c62256d90d1292
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-23 11:31:40 +0530

    update action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,19 @@
+import jsonplaceholder from '../apis/jsonPlaceholder';
+
 export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
+
+    //need to use redux-thunk for async actions inside action creators
+    //const response = await jsonplaceholder.get('/posts');  -> bad
+
+    // redux-thunk -> middleware to help us make requests from action creators
+
+    return function (dispatch, getState) {
+        const promise = jsonplaceholder.get('/posts');
+
+        return {
+            type: 'FETCH_POSTS',
+            payload: promise
+        }
     }
+
 }
