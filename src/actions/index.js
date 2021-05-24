92ef244a2e36d8abd5756d30fb2a22d741225896
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-24 11:45:49 +0530

    added fetchUser action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,4 +13,12 @@ export const fetchPosts = () => {
         dispatch({ type: 'FETCH_POSTS', payload: response.data })
     }
 
+};
+
+export const fetchUser = (id) => {
+    return async (dispatch) => {
+        const response = await jsonplaceholder.get(`/users/${id}`);
+
+        dispatch({ type: 'FETCH_USER', payload: response.data })
+    }
 }
