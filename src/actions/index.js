7e87494e995edc3654972fc6e555b63b9f38441d
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-25 10:54:43 +0530

    accessing unique userIds

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,11 @@ import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 
-export const fetchPostsAndUsers = () => async (dispatch) => {
-    dispatch(fetchPosts());
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    console.log(userIds);
 };
 
 export const fetchPosts = () => {
